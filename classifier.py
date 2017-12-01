17d1d859e82bf64ec6e5ce2f7fbf7a8ceb9af976
leanth <leanth@oregonstate.edu>
2017-11-30 20:48:56 -0800

    Add test 2 thru 8

--- classifier.py
+++ classifier.py
@@ -10,19 +10,14 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import callbacks
+from keras.preprocessing.image import ImageDataGenerator
 from sklearn.model_selection import StratifiedKFold
-
-# Stackoverflow to deal with python versions
-import io
 
 try:
     to_unicode = unicode
 except NameError:
     to_unicode = str
 
-    # from keras.models import Sequential
-
-# Tensorflow ordering of input data: samples, height, width, channel
 
 if __name__ == "__main__":
     with open('train.json') as data_file:
@@ -59,21 +54,30 @@ if __name__ == "__main__":
 
     images_test = np.array(images_test)
 
+    cvscores = []
+
+    datagen = ImageDataGenerator(width_shift_range=0.2, height_shift_range=0.2, fill_mode='nearest')
+    datagen.fit(images)
+
     model = Sequential()
 
     model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(75, 75, 3)))
+
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
 
     model.add(Conv2D(32, (3, 3), activation='relu'))
 
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
 
-    # model.add(Conv2D(64, (3, 3), activation='relu'))
-    # model.add(Conv2D(64, (3, 3), activation='relu'))
-    # model.add(MaxPooling2D(pool_size=(2, 2)))
-    # model.add(Dropout(0.25))
-
     model.add(Flatten())
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(64, activation='relu'))
+    model.add(Dropout(0.5))
+
     model.add(Dense(64, activation='relu'))
     model.add(Dropout(0.5))
 
@@ -84,13 +88,21 @@ if __name__ == "__main__":
 
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-    csv_logger = callbacks.CSVLogger("test1_epoch_results.log", separator=",", append=False)
-    model.fit(images, labels, validation_split=.10, batch_size=32, epochs=20, verbose=1, callbacks=[csv_logger])
+    csv_logger = callbacks.CSVLogger("test7_9lay_epoch_results.log", separator=",", append=False)
+    # model.fit(images, labels, validation_split=.10, batch_size=32, epochs=20, verbose=1, callbacks=[csv_logger])
+    model.fit_generator(datagen.flow(images, labels, batch_size=32), steps_per_epoch=len(images) / 32, epochs=20, verbose=1, callbacks=[csv_logger], shuffle=True)
+
+    scores = model.evaluate(images, labels, verbose=1)
+    print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
+    cvscores.append(scores[1] * 100)
 
     labels_test = model.predict(images_test, batch_size=32, verbose=1)
 
-    np.savetxt("test1_6lay_10stdval_default.csv", labels_test, delimiter=",")
+    np.savetxt("test7_9lay_default_predlabels.csv", labels_test, delimiter=",")
+    np.savetxt("test7_9lay_default_dev_acc.csv", cvscores, delimiter=",")
 
-    with open("ids.csv", "w") as idsfile:
-        wr = csv.writer(idsfile, dialect="excel")
-        wr.writerow(ids)
+    with open("test7_submission.csv", "w") as submission_file:
+        wr = csv.writer(submission_file, delimiter=",")
+        wr.writerow(["id", "is_iceberg"])
+        for i, p in zip(ids, labels_test):
+            wr.writerow((i, p[0]))
--- classifier_crossval.py
+++ classifier_crossval.py
@@ -11,19 +11,14 @@ from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.utils import plot_model
 from keras import callbacks
+from keras.preprocessing.image import ImageDataGenerator
 from sklearn.model_selection import StratifiedKFold
-
-# Stackoverflow to deal with python versions
-import io
 
 try:
     to_unicode = unicode
 except NameError:
     to_unicode = str
 
-    # from keras.models import Sequential
-
-# Tensorflow ordering of input data: samples, height, width, channel
 
 if __name__ == "__main__":
     with open('train.json') as data_file:
@@ -33,6 +28,7 @@ if __name__ == "__main__":
 
     images = []
     labels = []
+    inc_angles = []
     for j in data_loaded:
         first_channel = np.array(j["band_1"]).reshape(third_channel.shape)
         second_channel = np.array(j["band_2"]).reshape(third_channel.shape)
@@ -40,6 +36,10 @@ if __name__ == "__main__":
         image = np.rollaxis(image, 0, 3)
         images.append(image)
         labels.append(j["is_iceberg"])
+        inc_angles.append(j["inc_angle"])
+
+    # datagen = ImageDataGenerator(rotation_range=90)
+    # datagen.fit(images)
 
     with open("test.json") as test_file:
         test_loaded = json.load(test_file)
@@ -47,6 +47,7 @@ if __name__ == "__main__":
     third_channel_test = np.zeros((75, 75))
     images_test = []
     ids = []
+    inc_angles_test = []
     for k in test_loaded:
         first_channel_test = np.array(k["band_1"]).reshape(third_channel_test.shape)
         second_channel_test = np.array(k["band_2"]).reshape(third_channel_test.shape)
@@ -54,6 +55,7 @@ if __name__ == "__main__":
         image_test = np.rollaxis(image_test, 0, 3)
         images_test.append(image_test)
         ids.append(k["id"])
+        inc_angles_test.append(k["inc_angle"])
 
     images = np.array(images)
     labels = np.array(labels)
@@ -76,7 +78,7 @@ if __name__ == "__main__":
 
         model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(75, 75, 3)))
 
-        # model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
         # model.add(Dropout(0.25))
 
         model.add(Conv2D(32, (3, 3), activation='relu'))
@@ -84,28 +86,28 @@ if __name__ == "__main__":
         model.add(MaxPooling2D(pool_size=(2, 2)))
         model.add(Dropout(0.25))
 
-        # model.add(Conv2D(64, (3, 3), activation='relu'))
-        # model.add(Conv2D(64, (3, 3), activation='relu'))
-        # model.add(MaxPooling2D(pool_size=(2, 2)))
-        # model.add(Dropout(0.25))
-
         model.add(Flatten())
 
         model.add(Dense(64, activation='relu'))
-        model.add(Dropout(0.5))
+        model.add(Dropout(0.25))
 
         model.add(Dense(64, activation='relu'))
-        model.add(Dropout(0.5))
+        model.add(Dropout(0.25))
 
-        # model.add(Dense(64, activation='softplus'))
-        # model.add(Dropout(0.5))
+        model.add(Dense(64, activation='relu'))
+        model.add(Dropout(0.25))
+
+        model.add(Dense(64, activation='relu'))
+        model.add(Dropout(0.25))
 
         model.add(Dense(1, activation='sigmoid'))
 
         model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-        csv_logger = callbacks.CSVLogger("test2_6lay_10crossval_epoch_results.log", separator=",", append=True)
+        csv_logger = callbacks.CSVLogger("test8_9lay_10crossval_epoch_results.log", separator=",", append=True)
         model.fit(images[train], labels[train], batch_size=32, epochs=20, verbose=1, callbacks=[csv_logger])
+        # model.fit_generator(datagen.flow(images, labels, batch_size=32), steps_per_epoch=len(images) / 32, epochs=20,
+                            # verbose=1, callbacks=[csv_logger])
 
         scores = model.evaluate(images[test], labels[test], verbose=1)
         print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
@@ -114,21 +116,16 @@ if __name__ == "__main__":
     print("%.2f%% (+/- %.2f%%)" % (np.mean(cvscores), np.std(cvscores)))
 
     labels_test = model.predict(images_test, batch_size=32, verbose=1)
-    labels_test = np.array(labels_test)
-    labels_test = np.round(labels_test, decimals=1)
 
-    np.savetxt("test2_6lay_10crossval_predlabels.csv", labels_test, delimiter=",")
-    np.savetxt("test2_6lay_10crossval_dev_acc.csv", cvscores, delimiter=",")
+    np.savetxt("test8_9lay_10crossval_predlabels.csv", labels_test, delimiter=",")
+    np.savetxt("test8_9lay_10crossval_dev_acc.csv", cvscores, delimiter=",")
 
-    with open("test2_submission.csv", "w") as submission_file:
+    # with open("ids.csv", "w") as idsfile:
+    #     wr = csv.writer(idsfile, dialect="excel")
+    #     wr.writerow(ids)
+
+    with open("test8_submission.csv", "w") as submission_file:
         wr = csv.writer(submission_file, delimiter=",")
         wr.writerow(["id", "is_iceberg"])
         for i, p in zip(ids, labels_test):
-            wr.writerow((i, p))
-
-    # submission = []
-    #
-    # for i, p in zip(ids, labels_test):
-    #     submission.append((i, p))
-    #
-    # np.savetxt("test2_submission.csv", submission, delimiter=",")
+            wr.writerow((i, p[0]))
